cc93a143d5594fdc27ec001dd4f1e86f6bf1c1be
Hunter Davis <hdavis@rhapsody.com>
2015-02-18 10:33:21 -0800

    basic phase 1: named viewpager tabs displaying text relevant to that tab type

--- JSONResumeViewer/app/src/main/java/com/hunterdavis/jsonresumeviewer/BasicResumeFragment.java
+++ JSONResumeViewer/app/src/main/java/com/hunterdavis/jsonresumeviewer/BasicResumeFragment.java
@@ -19,19 +19,22 @@ public class BasicResumeFragment  extends Fragment {
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
 
+    private int sectionNumber = 0;
+
     /**
      * Returns a new instance of this fragment for the given section
      * number.
      */
-    public static BasicResumeFragment newInstance(int sectionNumber) {
+    public static BasicResumeFragment newInstance(ResumeSections sectionNumber) {
         BasicResumeFragment fragment = new BasicResumeFragment();
         Bundle args = new Bundle();
-        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber.ordinal());
         fragment.setArguments(args);
         return fragment;
     }
 
     public BasicResumeFragment() {
+
     }
 
     @Override
@@ -39,11 +42,69 @@ public class BasicResumeFragment  extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_json_resume, container, false);
 
+        Bundle arguments = getArguments();
+        sectionNumber = arguments.getInt(ARG_SECTION_NUMBER,0);
+
         TextView resumeText = (TextView) rootView.findViewById(R.id.resumeText);
-        if(JsonResumeActivity.resume != null) {
-            resumeText.setText(JsonResumeActivity.resume.toString());
-        }
+        resumeText.setText(getTextForSection(sectionNumber));
 
         return rootView;
+    }
+
+    public String getTextForSection(int sectionNumber) {
+        if(JsonResumeActivity.resume == null) {
+            return "";
+        }
+
+        ResumeSections section = ResumeSections.fromOrdinal(sectionNumber);
+
+        switch (section) {
+            case BASICS:
+                if(JsonResumeActivity.resume.getBasics() != null) {
+                    return JsonResumeActivity.resume.getBasics().toString();
+                }
+            case AWARDS:
+                if(JsonResumeActivity.resume.getAwards() != null) {
+                    return JsonResumeActivity.resume.getAwardsListTextually().toString();
+                }
+            case EDUCATION:
+                if(JsonResumeActivity.resume.getEducation() != null) {
+                    return JsonResumeActivity.resume.getEducationListTextually().toString();
+                }
+            case INTERESTS:
+                if(JsonResumeActivity.resume.getInterests() != null) {
+                    return JsonResumeActivity.resume.getInterestListTextually().toString();
+                }
+            case LANGUAGES:
+                if(JsonResumeActivity.resume.getLanguages() != null) {
+                    return JsonResumeActivity.resume.getLanguages().toString();
+                }
+            case PUBLICATIONS:
+                if(JsonResumeActivity.resume.getPublications() != null) {
+                    return JsonResumeActivity.resume.getPublicationsListTextually().toString();
+                }
+            case REFERENCES:
+                if(JsonResumeActivity.resume.getReferences() != null) {
+                    return JsonResumeActivity.resume.getReferencesListTextually().toString();
+                }
+            case SKILLS:
+                if(JsonResumeActivity.resume.getSkills() != null) {
+                    return JsonResumeActivity.resume.getSkillsListTextually().toString();
+                }
+            case VOLUNTEER:
+                if(JsonResumeActivity.resume.getVolunteer() != null) {
+                    return JsonResumeActivity.resume.getVolunteerListTextually().toString();
+                }
+            case WORK:
+                if(JsonResumeActivity.resume.getWork() != null) {
+                    return JsonResumeActivity.resume.getWorkListTextually().toString();
+                }
+            case PROFILES:
+                if(JsonResumeActivity.resume.getBasics() != null) {
+                    if(JsonResumeActivity.resume.getBasics().getProfiles() != null)
+                    return JsonResumeActivity.resume.getBasics().getProfilesListTextually();
+                }
+        }
+        return "";
     }
 }
--- JSONResumeViewer/app/src/main/java/com/hunterdavis/jsonresumeviewer/ResumePagerAdapter.java
+++ JSONResumeViewer/app/src/main/java/com/hunterdavis/jsonresumeviewer/ResumePagerAdapter.java
@@ -23,25 +23,43 @@ public class ResumePagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return BasicResumeFragment.newInstance(position + 1);
+        return BasicResumeFragment.newInstance(ResumeSections.fromOrdinal(position));
     }
 
     @Override
     public int getCount() {
         // Show 3 total pages.
-        return 3;
+        return 11;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
         Locale l = Locale.getDefault();
-        switch (position) {
-            case 0:
-                return adapterContext.getString(R.string.title_section1).toUpperCase(l);
-            case 1:
-                return adapterContext.getString(R.string.title_section2).toUpperCase(l);
-            case 2:
-                return adapterContext.getString(R.string.title_section3).toUpperCase(l);
+        ResumeSections section = ResumeSections.fromOrdinal(position);
+
+        switch (section) {
+            case BASICS:
+                return adapterContext.getString(R.string.title_basics).toUpperCase(l);
+            case AWARDS:
+                return adapterContext.getString(R.string.title_awards).toUpperCase(l);
+            case EDUCATION:
+                return adapterContext.getString(R.string.title_education).toUpperCase(l);
+            case INTERESTS:
+                return adapterContext.getString(R.string.title_interests).toUpperCase(l);
+            case LANGUAGES:
+                return adapterContext.getString(R.string.title_languages).toUpperCase(l);
+            case PUBLICATIONS:
+                return adapterContext.getString(R.string.title_publications).toUpperCase(l);
+            case REFERENCES:
+                return adapterContext.getString(R.string.title_references).toUpperCase(l);
+            case SKILLS:
+                return adapterContext.getString(R.string.title_skills).toUpperCase(l);
+            case VOLUNTEER:
+                return adapterContext.getString(R.string.title_volunteer).toUpperCase(l);
+            case WORK:
+                return adapterContext.getString(R.string.title_work).toUpperCase(l);
+            case PROFILES:
+                return adapterContext.getString(R.string.title_profiles).toUpperCase(l);
         }
         return null;
     }
