c4a62e201674506179292699eb66c1290fe88699
Hunter Davis <hdavis@rhapsody.com>
2015-02-18 15:45:45 -0800

    awards page

---
+++ JSONResumeViewer/app/src/main/java/com/hunterdavis/jsonresumeviewer/fragment/AwardsFragment.java
@@ -0,0 +1,113 @@
+package com.hunterdavis.jsonresumeviewer.fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.hunterdavis.jsonresumeviewer.JsonResumeActivity;
+import com.hunterdavis.jsonresumeviewer.R;
+import com.hunterdavis.jsonresumeviewer.types.Awards;
+
+import java.util.List;
+
+/**
+ * Created by hunter on 2/18/15.
+ */
+public class AwardsFragment extends ListFragment {
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static AwardsFragment newInstance() {
+        AwardsFragment fragment = new AwardsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public AwardsFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if(JsonResumeActivity.resume != null) {
+            if (JsonResumeActivity.resume.getAwards() != null) {
+                AwardsAdapter adapter = new AwardsAdapter(
+                        inflater.getContext(), JsonResumeActivity.resume.getAwards());
+                setListAdapter(adapter);
+            }
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    private class AwardsAdapter extends ArrayAdapter<Awards> {
+
+        public AwardsAdapter(Context context, List<Awards> items) {
+            super(context, R.layout.work_list_item, items);
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final WorkViewHolder viewHolder;
+            if (convertView == null) {
+                // inflate the GridView item layout
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(R.layout.award_list_item, parent, false);
+                // initialize the view holder
+                viewHolder = new WorkViewHolder();
+                viewHolder.awarder = (TextView) convertView.findViewById(R.id.awarder);
+                viewHolder.date = (TextView) convertView.findViewById(R.id.date);
+                viewHolder.summary = (TextView) convertView.findViewById(R.id.summary);
+                viewHolder.title = (TextView) convertView.findViewById(R.id.title);
+            } else {
+                // recycle the already inflated view
+                viewHolder = (WorkViewHolder) convertView.getTag();
+            }
+
+            // update the item view
+            final Awards item = getItem(position);
+
+            viewHolder.awarder.setText(item.getAwarder());
+            viewHolder.date.setText(item.getDate());
+            viewHolder.summary.setText(item.getSummary());
+            viewHolder.title.setText(item.getTitle());
+
+            return convertView;
+        }
+
+        private View.OnClickListener getGoToWebsiteOnclickListener(final String url) {
+            return new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(browserIntent);
+                }
+            };
+        }
+
+    }
+
+
+    /**
+     * convenience viewHolder pattern
+     */
+    private class WorkViewHolder {
+        TextView date;
+        TextView awarder;
+        TextView summary;
+        TextView title;
+
+    }
+}
